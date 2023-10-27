e41d9ad6731eea511e37acd5a01f592c016abff6
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-10-27 14:00:11 +0200

    Format code

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,3 +3,10 @@
 **Want to contribute? Great!** 
 
 First, read the [main contributing guide](https://github.com/quarkusio/quarkus/blob/main/CONTRIBUTING.md), it will informe you about the coding guidelines, the IDE configuration or the code style we use.
+
+To format your code make sure you run the following command before submitting a PR:
+
+```shell
+mvn net.revelc.code.formatter:formatter-maven-plugin:2.23.0:format
+mvn impsort:sort
+```
--- integration-tests/src/test/java/io/quarkiverse/semantickernel/it/SemanticKernelConfigurationTest.java
+++ integration-tests/src/test/java/io/quarkiverse/semantickernel/it/SemanticKernelConfigurationTest.java
@@ -19,10 +19,15 @@ public class SemanticKernelConfigurationTest {
 
     @Test
     public void testConfiguration() {
-        assertEquals(Optional.of("OPEN_AI_KEY"), configuration.client().flatMap(client -> client.openai().map(openai -> openai.key())));
-        assertEquals(Optional.of("OPEN_AI_ORGANIZATION_ID"), configuration.client().flatMap(client -> client.openai().map(openai -> openai.organizationid())));
-        assertEquals(Optional.of("AZURE_OPEN_AI_KEY"), configuration.client().flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.key())));
-        assertEquals(Optional.of("AZURE_OPEN_AI_ENDPOINT"), configuration.client().flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.endpoint())));
-        assertEquals(Optional.of("AZURE_OPEN_AI_DEPLOYMENT_NAME"), configuration.client().flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.deploymentname())));
+        assertEquals(Optional.of("OPEN_AI_KEY"),
+                configuration.client().flatMap(client -> client.openai().map(openai -> openai.key())));
+        assertEquals(Optional.of("OPEN_AI_ORGANIZATION_ID"),
+                configuration.client().flatMap(client -> client.openai().map(openai -> openai.organizationid())));
+        assertEquals(Optional.of("AZURE_OPEN_AI_KEY"),
+                configuration.client().flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.key())));
+        assertEquals(Optional.of("AZURE_OPEN_AI_ENDPOINT"),
+                configuration.client().flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.endpoint())));
+        assertEquals(Optional.of("AZURE_OPEN_AI_DEPLOYMENT_NAME"), configuration.client()
+                .flatMap(client -> client.azureopenai().map(azureopenai -> azureopenai.deploymentname())));
     }
 }
