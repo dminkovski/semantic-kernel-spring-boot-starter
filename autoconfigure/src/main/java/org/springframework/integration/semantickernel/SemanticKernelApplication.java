7f3a895f8e9536a2834f19fdfbc3189eb5b5f777
David Minkovski <minkovski.d@gmail.com>
2023-12-01 12:15:16 +0100

    Updated module structure according to: https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#features.developing-auto-configuration.custom-starter

--- autoconfigure/src/main/java/org/springframework/integration/semantickernel/SemanticKernelApplication.java
+++ autoconfigure/src/main/java/org/springframework/integration/semantickernel/SemanticKernelApplication.java
@@ -3,6 +3,7 @@ package org.springframework.integration.semantickernel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
 public class SemanticKernelApplication {
--- autoconfigure/src/main/java/org/springframework/integration/semantickernel/SemanticKernelConfiguration.java
+++ autoconfigure/src/main/java/org/springframework/integration/semantickernel/SemanticKernelConfiguration.java
@@ -14,31 +14,59 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @NoArgsConstructor
 @Configuration
-@ConfigurationProperties(prefix = "spring.semantic-kernel")
+@ConfigurationProperties(prefix = "semantic-kernel")
 public class SemanticKernelConfiguration {
-    
+
+    /**
+     * OpenAI and AzureOpenAI Configuration Wrapper
+     */
     private ClientConfig client;
+
+    /**
+     * Wrapper for Semantic Function Configuration
+     */
     private SemanticFunctionConfiguration semanticFunction;
 
     @Data
     @NoArgsConstructor
     public static class ClientConfig {
+        /**
+         * Open AI Configuration
+         */
         private OpenAIConfig openai;
+        /**
+         * Azure Open AI Configuration
+         */
         private AzureOpenAIConfig azureopenai;
     }
 
     @Data
     @NoArgsConstructor
     public static class OpenAIConfig {
+        /**
+         * OpenAI API Key
+         */
         private String key;
+        /**
+         * Open AI Organizational ID
+         */
         private String organizationid;
     }
 
     @Data
     @NoArgsConstructor
     public static class AzureOpenAIConfig {
+        /**
+         * Azure Open AI Endpoint URL
+         */
         private String endpoint;
+        /**
+         * Azure Open AI Key
+         */
         private String key;
+        /**
+         * Azure Open AI Deployment name
+         */
         private String deploymentname;
     }
 }
